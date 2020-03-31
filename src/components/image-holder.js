54ba8cea6b92317a1b42e40d0324384e183f2521
leny <info@flatland.be>
2020-03-31 21:55:57 +0200

    Basic implementation of drag'n'drop image holder

--- src/components/image-holder.js
+++ src/components/image-holder.js
@@ -6,16 +6,73 @@
  * started at 31/03/2020
  */
 
-import React from "react";
+import React, {useState, useCallback, useMemo} from "react";
 import {Image} from "react-bootstrap";
 
-const ImageHolder = () => (
-    <div className={"text-center"}>
-        <figure>
-            <Image src={"https://dummyimage.com/640x360/ddd/888"} thumbnail />
-            <figcaption>{"Drop an image to analyse"}</figcaption>
-        </figure>
-    </div>
-);
+const ImageHolder = () => {
+    const [isDraggedOver, setDraggedOver] = useState(false);
+    const [image, setImage] = useState(
+        "https://dummyimage.com/640x360/ddd/888",
+    );
+
+    const fileReader = useMemo(() => {
+        const reader = new FileReader();
+
+        reader.onload = () => setImage(reader.result);
+        reader.onerror = err => console.error(err);
+
+        return reader;
+    }, [setImage]);
+
+    const handleDragOver = useCallback(
+        e => {
+            e.preventDefault();
+            e.stopPropagation();
+            setDraggedOver(true);
+        },
+        [setDraggedOver],
+    );
+
+    const handleDragOut = useCallback(
+        e => {
+            e.preventDefault();
+            e.stopPropagation();
+            setDraggedOver(false);
+        },
+        [setDraggedOver],
+    );
+
+    const handleDrop = useCallback(e => {
+        e.preventDefault();
+        e.stopPropagation();
+        const [file] = e.dataTransfer.files;
+
+        fileReader.readAsDataURL(file);
+
+        setDraggedOver(false);
+    }, []);
+
+    return (
+        <div className={"text-center"}>
+            <figure
+                onDragOver={handleDragOver}
+                onDragEnter={handleDragOver}
+                onDragLeave={handleDragOut}
+                onDragEnd={handleDragOut}
+                onDrop={handleDrop}>
+                <Image
+                    style={{
+                        backgroundColor: isDraggedOver
+                            ? "orange"
+                            : "transparent",
+                    }}
+                    src={image}
+                    thumbnail
+                />
+                <figcaption>{"Drop an image to analyse"}</figcaption>
+            </figure>
+        </div>
+    );
+};
 
 export default ImageHolder;
